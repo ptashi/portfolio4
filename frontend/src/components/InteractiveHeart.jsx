62df8b7481f764626362e7ea2b13748f53699487
ptashi <pemalotus276@gmail.com>
2026-09-24 16:26:29 -0400

    addded the interactive cavas with pink hearts.

--- frontend/src/components/InteractiveHeart.jsx
+++ frontend/src/components/InteractiveHeart.jsx
@@ -4,18 +4,35 @@ import Canvas from "./Canvas"
 export default function InteractiveHeart({ role }) {
     const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
     const [trail, setTrail] = useState([])
+    const lastMoveRef = useRef(Date.now())
 
     const handleMouseMove = (event) => {
         const rect = event.target.getBoundingClientRect();
+        lastMoveRef.current = Date.now()
         setMousePosition({
             x: event.clientX - rect.left,
             y: event.clientY - rect.top,
         })
+
+        setTrail((prevTrail) => {
+            const maxHeart = 25
+            const newTrail= [
+                ...prevTrail,
+                {x: event.clientX - rect.left, y: event.clientY - rect.top, bornAt: Date.now()}
+            ]
+
+            if (newTrail.length > maxHeart) {
+                newTrail.shift() // removes first elemnt of array
+            }
+            return newTrail;
+        })
+
     }
 
     const drawHeart = (ctx, x, y, size, color) => {
-        ctx.save();
-        ctx.translate(x,y)
+
+        ctx.save(); // save the frame in time
+        ctx.translate(x,y) 
         ctx.fillStyle = color;
 
         ctx.beginPath()
@@ -45,11 +62,21 @@ export default function InteractiveHeart({ role }) {
         ctx.restore();
     }
     const draw = (ctx) => {
-        // ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-        for (i = 0; i < 20; i++) {
-            drawHeart(ctx, mousePosition.x, mousePosition.y, 20, 'pink')
-        }
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        
+
+        const DURATION = 300;
+        trail.forEach((point) => {
+            const age = Date.now() - point.bornAt;
+            if (age < DURATION) {
+                ctx.save();
+                drawHeart(ctx, point.x, point.y, 20, 'pink');
+                ctx.restore();
+            }
+        })
+
     }   
+
     return (
         <div className="relative min-h-screen min-w-screen bg-black">
             <Canvas
@@ -57,7 +84,7 @@ export default function InteractiveHeart({ role }) {
                 onMouseMove={handleMouseMove}
                 width={window.innerWidth}
                 height={window.innerHeight}
-                className="absolute inset-0"
+                className="absolute inset-0 block"
             />
 
             <div className="absolute inset-0 z-10 flex flex-col items-center justify-center text-center pointer-events-none">
